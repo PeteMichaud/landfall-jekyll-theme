04351279aad978ed64695518d2a6b1e4a9ee765c
Pete Michaud <michaudp@gmail.com>
2020-06-08 18:20:45 -0400

    added search

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,21 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
+
+  var pages = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: '/search.json'
+  });
+
+  $('#search-input').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 2
+      },
+      {
+        name: 'pages',
+        source: pages
+      });
+
 })
+
