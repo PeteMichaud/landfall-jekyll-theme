c4278ed883e284bc29c574265215b1c5f990c763
Pete Michaud <michaudp@gmail.com>
2020-06-08 16:37:21 -0400

    added search

--- assets/scripts.js
+++ assets/scripts.js
@@ -426,12 +426,3 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
-
-let sjs = SimpleJekyllSearch({
-  searchInput: document.getElementById('search-input'),
-  resultsContainer: document.getElementById('results-container'),
-  json: '/search.json',
-  searchResultTemplate: '<li><a href="{url}?query={query}" title="{desc}">{title}</a></li>',
-  noResultsText: 'No results found',
-  limit: 10,
-})
